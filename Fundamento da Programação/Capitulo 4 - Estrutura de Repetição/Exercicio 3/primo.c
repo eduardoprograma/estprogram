168882c698d833f3e349bbbafc4aba0f6cce06a4
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-09-21 16:39:42 -0300

    R00

--- Fundamento da Programação/Capitulo 4 - Estrutura de Repetição/Exercicio 3/primo.c
+++ Fundamento da Programação/Capitulo 4 - Estrutura de Repetição/Exercicio 3/primo.c
@@ -11,18 +11,18 @@ main()
 	scanf("%d", &N);
 	/*Bloco de comando*/
     while(N != -1){
-        if(2 <= N && N <= 21){
+        if(92 <= N && N <= 1478){
             while(CN < N){
                 CN++;
-                printf("\n%d\n\n", CN); //Fiz para poder conferir como o contador estava iniciando
+                //printf("\n%d\n\n", CN);       //Fiz para poder conferir como o contador estava iniciando
                 R = N % CN;
                 if(R == 0){
                     P++;
-                    printf("\n%dP\n", P);
+                    //printf("\n%dP\n", P);     // teste do contador
                 }
                 else if(R == 1){
                     NP++;
-                    printf("\n%d\n", NP);
+                    //printf("\n%d\n", NP);     //teste do contador
                 }
             }
             /*Saída dos dados*/
@@ -36,9 +36,9 @@ main()
         else{
             printf("Digite um numero entre 92 e 1478.\n\n");
         }
-        CN = 0;
-        P = 0;
-        NP = 0;
+        CN = 0;     //Zera Contador
+        P = 0;      //Zera Contador
+        NP = 0;     //Zera Contador
         printf("Digite um número: ");
 	    scanf("%d", &N);
     }
