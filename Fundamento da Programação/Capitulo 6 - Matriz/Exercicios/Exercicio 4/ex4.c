235ec2f9ca6e7bdc8823df36159a4ba2439cf013
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-10-27 11:17:34 -0300

    R00

--- Fundamento da Programação/Capitulo 6 - Matriz/Exercicios/Exercicio 4/ex4.c
+++ Fundamento da Programação/Capitulo 6 - Matriz/Exercicios/Exercicio 4/ex4.c
@@ -4,3 +4,69 @@
 Calcule:
 * Para cada aluno, o nome, a média aritmética das cinco provas e a situação(Aprovado, Exame ou Reprovado);
 * A média da classe.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*Definição dos elementos dos vetores e elementos para Matrizes.*/
+#define ALUNO 15
+#define NOTAS 5
+#define NOMEALUNO 60
+
+/*INICIO*/
+main()
+{
+    /*Localização*/
+    setlocale(LC_ALL, "portuguese");
+
+    /*Declaração de variáveis*/
+    char nomeAluno[ALUNO][NOMEALUNO], nome;
+    float notaAluno[ALUNO][NOTAS] = {0}, nota;
+    float mediaAluno = 0, mediaClasse = 0, soma = 0;
+    int i = 0, j = 0;
+
+    /*Entrada de dados*/
+    for(i = 0; i < ALUNO; i++)
+    {
+        printf("Digite o nome do aluno %d: ", i + 1);
+        scanf("%s", &nome);
+        nomeAluno[i][j] = nome;
+        printf("\n");
+        for(j = 0; j < NOTAS; j++)
+        {
+            printf("Digite a Nota da prova %d: ", j + 1);
+            scanf("%f", &nota);
+            notaAluno[i][j] = nota;
+        }
+        printf("\n");
+    }
+
+    /*Bloco de comando*/
+    printf("\n%s\t%20s\t%22s\t%15s\t\n", "Nome", "Notas", "Média", "Situação");
+    for(i = 0; i < ALUNO; i++)
+    {
+        printf("%s\t", nomeAluno[i]);
+        for(j = 0; j < NOTAS; j++)
+        {
+            soma += notaAluno[i][j];
+            printf("%.2f\t", notaAluno[i][j]);
+        }
+        mediaAluno = soma / 5;
+        printf("%.2f", mediaAluno);
+        if(mediaAluno >= 7)
+        {
+            printf("%20s\t\n", "Aprovado");
+        }
+        else if (mediaAluno >= 5 && mediaAluno < 7)
+        {
+            printf("%20s\t\n", "Exame");
+        }
+        else
+        {
+            printf("%20s\t\n", "Reprovado");
+        }
+        soma = 0;
+    }
+    system("pause");
+    return 0;    
+}
