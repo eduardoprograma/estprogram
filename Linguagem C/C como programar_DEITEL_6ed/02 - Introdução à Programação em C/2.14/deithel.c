be98999ef5cf4901def6d461e109f107c5845476
EDUARDO FELIZARDO CANDIDO <eduardo.candido@fatec.sp.gov.br>
2018-11-24 16:48:29 -0200

    R00

---
+++ Linguagem C/C como programar_DEITEL_6ed/02 - Introdução à Programação em C/2.14/deithel.c
@@ -0,0 +1,6 @@
+/*Dada a equação y = ax3 + 7, qual das instruções em C a seguir, se houver alguma, são corretas para ela? 
+a)y = a*x*x*x + 7; 
+b)y = a*x*x* (x+7); 
+c) y = (a * x) * x * (x + 7) ; 
+d) y = (a * x) * x * x + 7; 
+e) y = a * (x * x * x) + 7; f)y = a*x* (x * x + 7);*/
